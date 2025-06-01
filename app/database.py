6c06c8473fcbb31041eb16d7f5267b0fe1555063
IGONDIU <ion.gondiu@hotmail.com>
2025-06-02 01:53:04 +0200

    Change for in memory database

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
 from sqlmodel import Session, create_engine, SQLModel
+from sqlmodel.pool import StaticPool
 import settings as app_settings
 
 # echo=True prints all the SQL statements
-engine = create_engine(app_settings.SQLALCHEMY_DATABASE_URL, echo=True if app_settings.LOGS_LEVEL == "DEBUG" else False)
+engine = create_engine(
+    app_settings.SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+    echo=True if app_settings.LOGS_LEVEL == "DEBUG" else False,
+)
 
 
 def create_db_and_tables():
--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,13 @@
 import os
+import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+# Append base directory to the sys path so that imports are correctly working no matters from where the app is run.
+sys.path.insert(0, str(BASE_DIR))
 env_vars_loaded = load_dotenv(os.path.join(BASE_DIR, '.env'))
 if not env_vars_loaded:
     # Exit if env variables are not loaded from the .env file.
@@ -14,11 +17,14 @@ APP_NAME = "tickets_manager"
 ENV = os.environ.get("ENV", "DEV") # Default env is DEV
 
 # For in-memory database just two slash characters (//) and no file name:
-# SQLALCHEMY_DATABASE_URL = "sqlite://"
+SQLALCHEMY_DATABASE_URL = "sqlite://"
 
 # Persistent database -> all the data is saved in a file database.db
-SQLALCHEMY_DATABASE_URL = "sqlite:///database.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///database.db"
 
 LOGS_DIR = os.path.join(BASE_DIR, "logs")
+if not os.path.exists(LOGS_DIR):
+    os.makedirs(LOGS_DIR)
+
 LOGS_LEVEL = os.environ.get("LOGS_LEVEL", "DEBUG") # Default logs level is DEBUG
 
